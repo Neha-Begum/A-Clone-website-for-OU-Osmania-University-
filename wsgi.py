ce66cb20359a6d75fda57b3d9d47bb411a867b8f
Neha Begum <bneha8950@gmail.com>
2026-07-29 16:06:07 +0530

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('',include('results.urls')),
+    path('admin/', admin.site.urls),
+]
